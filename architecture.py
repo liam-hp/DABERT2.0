a1e69e15f2bcc34ced9c3e728320d56a9b5526b6
AlexisBallo2 <aballo@middlebury.edu>
2024-04-23 11:57:06 -0400

    our implementation

--- architecture.py
+++ architecture.py
@@ -17,8 +17,8 @@ class CustomBertModel(BertForMaskedLM):
             raise ValueError("BertForMaskedLM does not contain a 'bert' attribute with an encoder")
 
         for layer in encoder_layers:
-            #layer.attention.self = CustomAttention(config)
-            layer.attention.self = ActualBertSelfAttention(config)
+            layer.attention.self = CustomBertSelfAttention(config)
+            #layer.attention.self = ActualBertSelfAttention(config)
 
 
     def forward(self, input_ids, attention_mask=None, token_type_ids=None, labels=None, **kwargs):
@@ -29,14 +29,26 @@ class CustomBertModel(BertForMaskedLM):
             labels=labels  
         )
 
-class CustonBertSelfAttention(nn.Module):
+class CustomBertSelfAttention(nn.Module):
     def __init__(self, config):
-        super(CustonBertSelfAttention, self).__init__()
-        # print("init custom atteition layer", config)
-        self.query = nn.Linear(config.hidden_size, config.hidden_size)
-        self.key = nn.Linear(config.hidden_size, config.hidden_size)
-        self.value = nn.Linear(config.hidden_size, config.hidden_size)
-        self.combine = nn.Linear(config.hidden_size * 2, config.hidden_size)
+        super(CustomBertSelfAttention, self).__init__()
+
+        self.num_attention_heads = config.num_attention_heads
+        self.attention_head_size = int(config.hidden_size / config.num_attention_heads)
+        self.all_head_size = self.num_attention_heads * self.attention_head_size
+
+        # projection matracies into each dimension
+        # [hidden_size] -> [num_attention_heads x head_size]
+        self.query = nn.Linear(config.hidden_size, self.all_head_size)
+        self.key = nn.Linear(config.hidden_size, self.all_head_size)
+        self.value = nn.Linear(config.hidden_size, self.all_head_size)
+
+        self.linearLayer = nn.Linear(self.attention_head_size * 3, self.attention_head_size)
+
+    def transpose_for_scores(self, x: torch.Tensor) -> torch.Tensor:
+        new_x_shape = x.size()[:-1] + (self.num_attention_heads, self.attention_head_size)
+        x = x.view(new_x_shape)
+        return x.permute(0, 2, 1, 3)
 
     def forward(self, 
                 hidden_states, 
@@ -44,11 +56,40 @@ class CustonBertSelfAttention(nn.Module):
                 head_mask=None, 
                 encoder_hidden_states=None, 
                 encoder_attention_mask=None, past_key_value=None, output_attentions=None):
-        query_layer = self.query(hidden_states)
-        value_layer = self.value(hidden_states)
-        combined = torch.cat((query_layer, value_layer), dim=-1)  # Ensure correct dimension
-        output = self.combine(combined)
-        return (output, output)
+
+        mixed_query_layer = self.query(hidden_states)
+
+        # projects each kvq into a different head 
+        # [batch_size x sen_length x hidden_size] 
+        # -> 
+        # [batch_size x num_heads x sen_length x head_size]
+        key_layer = self.transpose_for_scores(self.key(hidden_states))
+        value_layer = self.transpose_for_scores(self.value(hidden_states))
+        query_layer = self.transpose_for_scores(mixed_query_layer)
+
+        # combine on the last dimension
+        combined = torch.cat((key_layer, query_layer, value_layer), dim=3) 
+
+        # [batch_size x num_heads x sen_length x head_size]
+        output = self.linearLayer(combined)
+
+        # Begin transofrmation to original shape
+
+        # 1) Reorder the tensor to [batch_size x sen_length x num_heads x head_size]
+        context_layer = output.permute(0, 2, 1, 3).contiguous()
+
+        # 2) Computer output format: [batch_size x sen_length x hidden_size]
+        #    context_layer.size()[:-2] gets [batch_size x sen_length] 
+        #    self.all_head_size is the hidden_size
+        new_context_layer_shape = context_layer.size()[:-2] + (self.all_head_size,)
+
+        # 3) Reshape context to: [batch_size x sen_length x hidden_size]
+        context_layer = context_layer.view(new_context_layer_shape)
+
+        # Special output format specified by origional Bert implementation
+        outputs = (context_layer,)
+        # Reshape context to: [batch_size x sen_length x hidden_size]
+        return outputs
 
 
 
@@ -119,7 +160,7 @@ class ActualBertSelfAttention(nn.Module):
         #    self.all_head_size is the hidden_size
         new_context_layer_shape = context_layer.size()[:-2] + (self.all_head_size,)
 
-        # 3) Reshape context to: batch_size x sen_length x hidden_size]
+        # 3) Reshape context to: [batch_size x sen_length x hidden_size]
         context_layer = context_layer.view(new_context_layer_shape)
 
 
