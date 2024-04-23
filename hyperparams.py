da0258a5612c098c30683e89e502a65bc3c6774b
Patty <wpatty@ada.middlebury.edu>
2024-04-22 20:36:24 -0400

    hyperparameters

---
+++ hyperparams.py
@@ -0,0 +1,14 @@
+hyperparams = {
+  "include_key_matrix": NotImplemented,
+  "transfer_learning": NotImplemented,
+  "DNN_layers": 1,
+  "predictions_per_mask": 20,
+  "masking_rate": .15,
+  "context-size": NotImplemented,
+  "next_sentence_prediction": NotImplemented,
+  "disable_bias": False,
+  "num_encoding_heads": 12,
+  "early_stopping": False,
+  "epochs": 10,
+  "attention_type": "DNN",
+}
