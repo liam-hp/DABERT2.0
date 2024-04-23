f8b122820cadc372fc68a9b338599b5e1673b411
Patty <wpatty@ada.middlebury.edu>
2024-04-23 14:59:04 -0400

    fixing

--- main.py
+++ main.py
@@ -26,21 +26,25 @@ print(f"Setting device... {device}")
 
 def train():
     
+    epochs = hyperparams.get["epochs"]
+    batch_size = hyperparams.get["batch_size"]
+    max_sent_len = hyperparams.get["max_sentence_len"]
+    test_epochs = hyperparams.get["test_epochs"]
+
     print("Initializing config, model, and optimizer...")
     config = BertConfig.from_pretrained('bert-base-uncased')
     model = architecture.CustomBertModel(config).to(device)
     optimizer = optim.AdamW(model.parameters(), lr=2e-5)
 
-    train_dataloader = batching.get_data_loader(trainSentences, batch_size=hyperparams.get["batch_size"], max_length=hyperparams.get["max_sentence_len"])
-    test_dataloader = batching.get_data_loader(trainSentences, batch_size=hyperparams.get["batch_size"], max_length=hyperparams.get["max_sentence_len"])
+    train_dataloader = batching.get_data_loader(trainSentences, batch_size=batch_size, max_length=max_sent_len)
+    test_dataloader = batching.get_data_loader(trainSentences, batch_size=batch_size, max_length=max_sent_len)
 
     # training
-    training_size = hyperparams.get['epochs']*hyperparams.get['batch_size']
-    print(f"Beginning training on {training_size} example sentences (approx. {round(hyperparams.get['epochs']/len(train_dataloader), 2)}% of available)...")
+    print(f"Beginning training on {epochs*batch_size} example sentences (approx. {round(epochs / len(train_dataloader), 2)}% of available)...")
     
     start_time = datetime.datetime.now()
     losses = []
-    for epoch in range(hyperparams.get["epochs"]):
+    for epoch in range(epochs):
       optimizer.zero_grad()
       batch = next(iter(train_dataloader))# batch is a dict of keys: input_ids, token_type_ids, attention_mask, labels
       batch = {k: v.to(device) for k, v in batch.items()} # move batch to the appropriate device
@@ -49,22 +53,23 @@ def train():
       losses.append(loss.item())
       loss.backward()
       optimizer.step()
-      print(f'\t Epoch {epoch} | Loss: {loss.item():.4f}')
+      if(100 * epoch / epochs % 5 == 0):
+        print(f'\t {100 * epoch / epochs}% | Epoch {epoch} | Loss: {loss.item():.4f}')
+    print(f'\t 100% | Epoch {epoch} | Loss: {loss.item():.4f}')
     
-    training_time = datetime.timedelta((datetime.datetime.now()-start_time).total_seconds())
-    print(f"Training finished. Total training time: {training_time}")
+    training_time = datetime.timedelta(seconds=(datetime.datetime.now()-start_time).total_seconds())
+    print(f"Training finished. Total training time (H:mm:ss): {training_time}")
 
     # validation
     avg_val_loss = 0
-    validation_size = hyperparams.get['test_epochs']*hyperparams.get['batch_size']
-    print(f"Beginning validation on {validation_size} example sentences (approx. {round(hyperparams.get['test_epochs']/len(test_dataloader), 2)}% of available)...")
-    for epoch in range(hyperparams.get["test_epochs"]):
+    print(f"Beginning validation on {test_epochs*batch_size} example sentences (approx. {round(test_epochs/len(test_dataloader), 2)}% of available)...")
+    for epoch in range(test_epochs):
       batch = next(iter(test_dataloader))# batch is a dict of keys: input_ids, token_type_ids, attention_mask, labels
       batch = {k: v.to(device) for k, v in batch.items()} # move batch to the appropriate device
       outputs = model(**batch) # unpack the batch dictionary directly into the model
       loss = outputs.loss # the loss is returned when 'labels' are provided in the input
       avg_val_loss += loss.item()
-    avg_val_loss /= hyperparams.get['test_epochs']
+    avg_val_loss /= test_epochs
     print(f"Validation complete. Avg validation loss: {avg_val_loss}")
 
     return 
